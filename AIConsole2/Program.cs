bf5251d81ee92928ff237575ecb6d87424a28ffc
Administrator <Administrator@DESKTOP-95NRM61>
2026-06-18 00:44:59 +0800

    111

---
+++ AIConsole2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.Extensions.AI;
+using System.ClientModel;
+IChatClient client =
+    new OpenAI.Chat.ChatClient("mimo-v2.5-pro", new ApiKeyCredential("tp-ctqrehoezox60yzmkcolsskgvxsajdduazi14i0aazu7zp26"), new OpenAI.OpenAIClientOptions { Endpoint = new Uri("https://token-plan-cn.xiaomimimo.com/v1/") })
+    .AsIChatClient();
+
+List<ChatMessage> chatmessages = [
+    new ChatMessage
+{
+    Role = ChatRole.System
+}];
+client.GetStreamingResponseAsync()
