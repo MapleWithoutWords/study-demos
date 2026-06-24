14bfa7929f9d9586379b449a5dbe8432a4dd0370
Administrator <Administrator@DESKTOP-95NRM61>
2026-06-25 02:03:31 +0800

    feat:完成mcp案例

---
+++ AIMCPCallingConsole5/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using AI.Common;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using ModelContextProtocol.Client;
+using System.ClientModel;
+using System.Text.Json;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets(typeof(AICommon).Assembly)
+    .Build();
+
+string baseUrl = configuration.GetSection("AIEndpoint").Value!;
+string apiKey = configuration.GetSection("AIApiKey").Value!;
+const string model = "glm-5.1";
+
+HttpClientInterceptor.StartInterception();
+
+IChatClient client =
+    new OpenAI.Chat.ChatClient(model, new ApiKeyCredential(apiKey), new OpenAI.OpenAIClientOptions { Endpoint = new Uri(baseUrl) })
+    .AsIChatClient();
+
+// 配置传输选项
+
+var config = new StdioClientTransport(
+    new StdioClientTransportOptions()
+    {
+        Command = "dnx",
+        Arguments = ["NuGet.Mcp.Server", "--source", "https://api.nuget.org/v3/index.json", "--yes"]
+    }
+);
+
+// 创建 MCP 客户端实例
+var mcpClient = await McpClient.CreateAsync(config);
+
+var tools = await mcpClient.ListToolsAsync();
+foreach (var item in tools)
+{
+    Console.WriteLine(item.Name);
+}
+
+using var functionCallingChatClient = new Microsoft.Extensions.AI.ChatClientBuilder(client)
+    .UseFunctionInvocation()
+    .Build();
+
+while (true)
+{
+    Console.Write("Prompt: ");
+    List<ChatMessage> messages = [];
+    messages.Add(new(ChatRole.User, Console.ReadLine()));
+
+    await foreach (ChatResponseUpdate update in functionCallingChatClient
+        .GetStreamingResponseAsync(messages, new() { Tools = [.. tools] }))
+    {
+        foreach (var item in update.Contents)
+        {
+            if (item is TextReasoningContent textReasoning)
+            {
+                Console.Write(textReasoning.Text);
+            }
+            else if (item is TextContent text)
+            {
+                Console.Write(text.Text);
+            }
+            else if (item is UsageContent usageContent)
+            {
+                Console.WriteLine();
+                Console.WriteLine(JsonSerializer.Serialize(usageContent.Details));
+            }
+        }
+    }
+    Console.WriteLine();
+
+}
